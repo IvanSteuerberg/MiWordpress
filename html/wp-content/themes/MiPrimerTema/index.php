66a7f064329b0333d3e5ae010921b8a7f813a3ee
ivansteuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-11-25 11:50:49 +0100

    Variable count

--- html/wp-content/themes/MiPrimerTema/index.php
+++ html/wp-content/themes/MiPrimerTema/index.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
 	<?php wp_head(); ?>
 </head>
 <body>
 <?php get_header() ?> <!-- Obtenemos el header -->
 <h1><?php bloginfo( 'name' ); ?></h1>
 <h2><?php bloginfo( 'description' ); ?></h2>
-
+<?php $count = 0; ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php title_with_link(); ?>
-    <?php if ( has_post_thumbnail() ) {
-    the_post_thumbnail('medium');
-    }?>
-	<?php the_excerpt(); ?>
-	<?php wp_link_pages(); ?>
-	<?php edit_post_link(); ?>
+    <?php $count++; ?>
+	<?php
+    title_with_link();
+    if ( has_post_thumbnail() ) {
+		the_post_thumbnail('medium');
+	}
+	the_excerpt();
+	wp_link_pages();
+	edit_post_link();
+	?>
 
 <?php endwhile; ?>
 
@@ -35,7 +38,7 @@
 
 <?php else: ?>
 
-	<p>No posts found. :(</p>
+    <p>No posts found. :(</p>
 
 <?php endif; ?>
 <?php get_footer() ?> <!-- Obtenemos el footer -->
