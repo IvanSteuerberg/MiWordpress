83ce96a8b57ef397fe851e2204e15e94275125ee
ivansteuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-12-01 10:02:44 +0100

    Cambios en 404.php

--- html/wp-content/themes/twentytwenty-child/404.php
+++ html/wp-content/themes/twentytwenty-child/404.php
@@ -13,7 +13,12 @@ get_header();
 <main id="site-content" role="main">
 
 	<div class="section-inner thin error404-content">
-
+        <div style="background-color: #5d01ff;color: #00a0d2">
+        <br>
+        <h1>:(</h1>
+        <br>
+        </div>
+        <br>
 		<h2 class="entry-title" style="background-color: #ffd800"><?php _e( 'Page Not Found', 'twentytwenty' ); ?></h2>
 
 		<div class="intro-text"><p style="background-color: #00a0d2;color: #ffffff"><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
